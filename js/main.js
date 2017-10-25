3b14b3fcd536d68e0fac1181b790389c7ad422f3
Calvin Atlan <calvinatlan@gmail.com>
2017-10-24 22:35:44 -0700

    Add all videos and make the div scroll

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+/*jshint esversion: 6 */
 main = function(){
-    console.log('ready');
+    let videoLength = document.querySelectorAll('.section.videos div iframe').length;
+    let section = document.querySelector('.section.videos .iframe-container');
+    console.log(section);
+    section.style.width = (videoLength * (560 + 25)) + 'px';
 };
 window.onload = main;
