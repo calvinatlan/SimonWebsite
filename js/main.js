eca43c4dcf898c36f612871ba84328dfd043374b
Calvin Atlan <calvinatlan@gmail.com>
2017-10-24 23:33:39 -0700

    Create credits section and link

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
 /*jshint esversion: 6 */
 main = function(){
+    setVideoLength();
+};
+
+//TODO scroll down makes header transparent, use RXJS
+let scrollHideHeader = function(){
+
+};
+
+let setVideoLength = function(){
     let videoLength = document.querySelectorAll('.section.videos div iframe').length;
     let section = document.querySelector('.section.videos .iframe-container');
-    console.log(section);
     section.style.width = (videoLength * (560 + 25)) + 'px';
 };
+
 window.onload = main;
